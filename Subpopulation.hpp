225c31b5198408daeee385e41383104418e588e2
msupernaw <matthew.supernaw@noaa.gov>
2014-01-07 10:14:04 -0500

    Create Subpopulation.hpp

---
+++ Subpopulation.hpp
@@ -0,0 +1,29 @@
+#ifndef SUBPOPULATION_HPP
+#define	SUBPOPULATION_HPP
+
+/**
+ *Used to define the location of a subpopulation.
+ */
+template<class T>
+class Polygon {
+};
+
+template<class T>
+class Movement {
+};
+
+template<class T>
+class Structure {
+};
+
+template<class T>
+class Subpopulation {
+    
+    Movement<T>* movement_m;
+    Structure<T>* structure_m;
+    Polygon<T>* polygon_m;
+    
+    public:
+    
+
+};
